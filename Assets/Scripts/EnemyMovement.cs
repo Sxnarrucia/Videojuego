eab0d0899ed51aff7e3584544c09ffbfd123f4fb
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2024-11-20 23:34:37 -0600

    Enemy Added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    public int patrolDestination;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime); 
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < 1.0f)
+            {
+                transform.localScale = new Vector3(3,3,1);
+                patrolDestination = 1;
+            } 
+        }
+
+                if (patrolDestination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime); 
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < 1.0f)
+            {
+                transform.localScale = new Vector3(-3,3,1);
+                patrolDestination = 0;
+            } 
+        }
+    }
+}
